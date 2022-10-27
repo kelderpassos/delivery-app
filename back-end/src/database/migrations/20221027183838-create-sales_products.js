2cb36983071ef243e91e6fa3d960423d64c9f842
Kelder Passos <kelder.passos@gmail.com>
2022-10-27 16:02:41 -0300

    main-group-23 feat: migrations created

---
+++ back-end/src/database/migrations/20221027183838-create-sales_products.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("sales_products", {
+      sale_id: {
+        allowNull: false,
+        type: Sequelize.VARCHAR,
+        references: { model: "sales", key: "id" },
+        onDelete: 'CASCADE'
+      },
+      product_id: {
+        allowNull: false,
+        type: Sequelize.VARCHAR,
+        references: { model: "products", key: "id" },
+        onDelete: 'CASCADE'
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("sales_products");
+  },
+};
