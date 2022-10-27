a3e61edffa4af4aaf2bc4af3d224abfe9bda7f61
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-27 17:44:37 -0300

    grupo 23 - fix: migrations

--- back-end/src/database/migrations/20221027183622-create-user.js
+++ back-end/src/database/migrations/20221027183622-create-user.js
@@ -11,20 +11,20 @@ module.exports = {
       },
       name: {
         allowNull: false,
-        type: Sequelize.VARCHAR,
+        type: Sequelize.STRING,
       },
       email: {
         allowNull: false,
-        type: Sequelize.VARCHAR,
+        type: Sequelize.STRING,
         unique: true,
       },
       password: {
         allowNull: false,
-        type: Sequelize.VARCHAR,
+        type: Sequelize.STRING,
       },
       role: {
         allowNull: false,
-        type: Sequelize.VARCHAR,
+        type: Sequelize.STRING,
       },
     })
   },
