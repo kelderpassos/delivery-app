dd03df2617c75f1b4f43aebe8154bb12dbd33d73
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-28 09:34:03 -0300

    feat: App com cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('../routes/user.route');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(userRoute);
 
