8338955fc01350f41dff419ff719a6e33ba6ad5f
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-28 08:22:26 -0300

    feat: app.js e server.js - adiciona o uso de UserRoute

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const userRoute = require('../routes/user.route');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(userRoute);
 
 module.exports = app;
--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const port = process.env.PORT || 3001;
 const app = require('./app');
 
 app.listen(port);
-console.log(`Api rodando na porta teste ${port}`);
+console.log(`Api rodando na porta ${port}`);
