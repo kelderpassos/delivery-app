ea6543d4d81dc12cbd247e323d4223d61af3f745
Kelder Passos <kelder.passos@gmail.com>
2022-10-28 17:12:50 -0300

    feat: function to fetch all products from database is ready

---
+++ back-end/src/controller/product.controller.js
@@ -0,0 +1,9 @@
+const productService = require("../services/product.service");
+
+const getAllProducts = async (_req, res) => {
+  const allProducts = await productService.getAllProducts();
+
+  return res.status(200).json(allProducts);
+};
+
+module.exports = { getAllProducts };
