0db7096374d846683dbe71c86ea6f0b4c15302dd
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-28 08:49:52 -0300

    fix: app.js - Agora utiliza express.json();

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,8 @@ const userRoute = require('../routes/user.route');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(userRoute);
