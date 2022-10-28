c3504fc40ecb832b364ea3a9ba3a8afad96ea7e1
Kelder Passos <kelder.passos@gmail.com>
2022-10-28 20:37:24 -0300

    Feat: adding data-testids

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,16 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   const [loggedUser, setLoggedUser] = useState('');
+  console.log(setLoggedUser());
 
   return (
     <header>
       <nav>
-        <NavLink to="/products">Produtos</NavLink>
-        <NavLink to="/orders">Meus Pedidos</NavLink>
-        <NavLink to="/user">{loggedUser}</NavLink>
-        <NavLink to="/logout">Sair</NavLink>
+        <NavLink
+          data-testid="customer_products__element-navbar-link-products"
+          to="/products"
+        >
+          Produtos
+        </NavLink>
+        <NavLink
+          data-testid="customer_products__element-navbar-link-orders"
+          to="/orders"
+        >
+          Meus Pedidos
+        </NavLink>
+        <NavLink
+          data-testid="customer_products__element-navbar-user-full-name"
+          to="/user"
+        >
+          {loggedUser}
+        </NavLink>
+        <NavLink
+          data-testid="customer_products__element-navbar-link-logout"
+          to="/logout"
+        >
+          Sair
+        </NavLink>
       </nav>
     </header>
   );
