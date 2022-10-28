21b4e4d84a1af983a4deee5fad09b2ddd2a58489
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-28 09:14:51 -0300

    fix: Tipo de requisição para /login - De GET para POST

--- back-end/src/routes/user.route.js
+++ back-end/src/routes/user.route.js
@@ -4,7 +4,7 @@ const userController = require('../controller/user.controller');
 
 const userRoute = express.Router();
 
-userRoute.get('/login', rescue((req, res, _next) =>
+userRoute.post('/login', rescue((req, res, _next) =>
   userController.login(req, res)));
 
 module.exports = userRoute;
