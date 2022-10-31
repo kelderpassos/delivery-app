1517ce91df786bb61adfc1527ffa8e68d03abfe6
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-10-31 18:42:13 -0300

    feat: GET /sellers - Rota para buscar o nome de todos s vendedores.

--- back-end/src/controller/user.controller.js
+++ back-end/src/controller/user.controller.js
@@ -11,7 +11,12 @@ const customerRegister = async (req, res) => {
   const { name, email, password } = req.body;
   const newUser = await userService.customerRegister({ name, email, password });
 
-  res.status(201).json(newUser);
+  return res.status(201).json(newUser);
 };
 
-module.exports = { login, customerRegister };
+const findSellersNames = async (_req, res) => {
+  const sellers = await userService.findSellersNames();
+  return res.status(200).json(sellers);
+};
+
+module.exports = { login, customerRegister, findSellersNames };
