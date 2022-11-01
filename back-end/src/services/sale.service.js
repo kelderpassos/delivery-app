ad704f55175d2e10c5a2fcc799a0cacb7d39a8ff
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-01 17:02:34 -0300

    feat: post /salles - Rota para registro de novas vendas

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,21 +1,26 @@
 const models = require('../database/models');
+const saleProductsService = require('./saleProducts.service');
 
 const { Sale, sequelize } = models;
 
-// const create = async (
-//   { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products },
-// ) => {
-//   const newSale = await sequelize.transaction(async (transaction) => {
-//     const created = await Sale.create(
-//      { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status },
-//      { transaction },
-//     );
+const create = async (
+  { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products },
+) => {
+  const newSale = await sequelize.transaction(async (transaction) => {
+    const created = await Sale.create(
+     { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status },
+     { transaction },
+    );
+
+    const { id } = created;
+    const saleProducts = products.map((product) => ({ ...product, saleId: id }));
     
-//     console.log(created.id);
-//     return created.id;
-//   });
+    await saleProductsService.bulkCreate(saleProducts);
+    
+    return id;
+  });
 
-//   return newSale;
-// };
+  return newSale;
+};
 
-module.exports = { };
+module.exports = { create };
