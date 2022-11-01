b7beb747b32cfcf06090ac8ab73e978c7f31024f
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-01 19:40:04 -0300

    feat: Validação JWT

--- back-end/src/controller/sale.controller.js
+++ back-end/src/controller/sale.controller.js
@@ -1,17 +1,20 @@
 const saleService = require('../services/sale.service');
+const jwt = require('../auth/JWT');
 
 const create = async (req, res) => {
   const {
     name, sellerId, totalPrice, deliveryAddress, deliveryNumber, products,
   } = req.body;
 
-  const status = 'Pendente';
+  const token = req.headers.authorization;
+
+  jwt.validate(token);
 
   const newOrderId = await saleService.create(
-    { name, sellerId, totalPrice, deliveryAddress, deliveryNumber, status, products },
+    { name, sellerId, totalPrice, deliveryAddress, deliveryNumber, products },
   );
 
-  res.status(201).json({ newOrderId });
+  return res.status(201).json({ newOrderId });
 };
 
 module.exports = { create };
