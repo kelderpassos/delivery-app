da1220d8abf7faa5da6030d241322a5a2e75d575
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-03 19:19:37 -0300

    feat: SellerOrders - Requisição a API funcionando.

--- front-end/src/pages/SellerOrders.jsx
+++ front-end/src/pages/SellerOrders.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import NavBar from '../components/NavBar';
 
 export default function SellerOrders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+
+    const config = {
+      headers: {
+        authorization: token,
+      },
+    };
+
+    axios.get('http://localhost:3001/seller/orders', config)
+      .then((result) => result.data)
+      .then((data) => setOrders(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(orders);
+
   return (
-    <p>seller order</p>
+    <div>
+      <NavBar />
+      <p>ta de casas</p>
+    </div>
   );
 }
