bf190feb500c62bcc315b99dde0ddd7b4e7550f2
Kelder Passos <kelder.passos@gmail.com>
2022-11-03 17:26:44 -0300

    Feat: req 24 ready

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,28 +1,36 @@
-const models = require('../database/models');
-const CustomError = require('../helpers/CustomError');
-const saleProductsService = require('./saleProducts.service');
-const userService = require('./user.service');
+const { Sale, User, sequelize } = require("../database/models");
+const CustomError = require("../helpers/CustomError");
+const saleProductsService = require("./saleProducts.service");
+const userService = require("./user.service");
 
-const { Sale, sequelize } = models;
+// const { Sale, sequelize } = models;
 
-const create = async (
-  { name, sellerId, totalPrice, deliveryAddress, deliveryNumber, products },
-) => {
+const create = async ({
+  name,
+  sellerId,
+  totalPrice,
+  deliveryAddress,
+  deliveryNumber,
+  products,
+}) => {
   const newSale = await sequelize.transaction(async (transaction) => {
     const { id: userId } = await userService.findByName({ name });
 
-    const status = 'Pendente';
+    const status = "Pendente";
 
     const created = await Sale.create(
-     { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status },
-     { transaction },
+      { userId, sellerId, totalPrice, deliveryAddress, deliveryNumber, status },
+      { transaction }
     );
 
     const { id } = created;
-    const saleProducts = products.map((product) => ({ ...product, saleId: id }));
-    
+    const saleProducts = products.map((product) => ({
+      ...product,
+      saleId: id,
+    }));
+
     await saleProductsService.bulkCreate(saleProducts);
-    
+
     return id;
   });
 
@@ -30,12 +38,16 @@ const create = async (
 };
 
 const getById = async (id) => {
-  const specificSale = await Sale.findByPk(id);
+  const specificSale = await Sale.findByPk(id, {
+    include: [
+      { model: User, as: "seller" },
+      { model: User, as: "users" },
+    ],
+  });
 
-  if (!specificSale) throw new CustomError(404, 'sale not found');
+  if (!specificSale) throw new CustomError(404, "sale not found");
 
   return specificSale;
-}
-
+};
 
 module.exports = { create, getById };
