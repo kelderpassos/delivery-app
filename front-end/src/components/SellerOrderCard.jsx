b201a5f35f8c36c9d3417ac311bd9c4940232025
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-04 08:46:02 -0300

    feat: SellerOrderCard - Componente para exibição dos pedidos de um vendedor

---
+++ front-end/src/components/SellerOrderCard.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const TEN = 10;
+
+export default function SellerOrderCard({
+  id, num, totalPrice, deliveryAddress, deliveryNumber, saleDate, status,
+}) {
+  const total = totalPrice.replace('.', ',');
+  const date = saleDate.slice(0, TEN).replace(/-/g, '/');
+
+  return (
+    <div>
+      <div>
+        <p data-testid={ `seller_orders__element-order-id-${id}` }>{ num }</p>
+      </div>
+      <div>
+        <p data-testid={ `seller_orders__element-delivery-status-${id}` }>{ status }</p>
+        <div>
+          <p data-testid={ `seller_orders__element-order-date-${id}` }>
+            { date }
+          </p>
+          <p data-testid={ `seller_orders__element-card-price-${id}` }>
+            { `R$ ${total}` }
+          </p>
+        </div>
+        <p data-testid={ `seller_orders__element-card-address-${id}` }>
+          { `${deliveryAddress}, ${deliveryNumber}` }
+        </p>
+      </div>
+    </div>
+  );
+}
+
+SellerOrderCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  num: PropTypes.number.isRequired,
+  totalPrice: PropTypes.string.isRequired,
+  deliveryAddress: PropTypes.string.isRequired,
+  deliveryNumber: PropTypes.string.isRequired,
+  saleDate: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
