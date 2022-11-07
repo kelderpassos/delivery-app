0499f69c3e48d4cb64e6bb8a05b55a1a02bd24a0
LucasLimaPE <robertolucas011@gmail.com>
2022-11-07 15:44:43 -0300

    feat: componente SellerDetailsTable Componente que renderiza a tabela de detalhes da venda para o vendedor.

---
+++ front-end/src/components/SellerDetailsTable.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function SellerDetailsTable({ allProducts }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Description</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        {allProducts.map((product, ind) => {
+          const { quantity } = product.saleProducts[0];
+          const totalPrice = (product.price * quantity).toFixed(2).replace('.', ',');
+          return (
+            <tr key={ ind }>
+              <td
+                data-testid={ `seller_order_details__
+                element-order-table-item-number-${ind}` }
+              >
+                { ind + 1 }
+              </td>
+              <td
+                data-testid={ `seller_order_details__element-order-table-name-${ind}` }
+              >
+                { product.name }
+              </td>
+              <td
+                data-testid={ `seller_order_details__
+                element-order-table-quantity-${ind}` }
+              >
+                { quantity }
+              </td>
+              <td
+                data-testid={ `seller_order_details__
+                element-order-table-unit-price-${ind}` }
+              >
+                { product.price.replace('.', ',') }
+              </td>
+              <td
+                data-testid={ `seller_order_details__
+                element-order-table-sub-total-${ind}` }
+              >
+                { totalPrice }
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+SellerDetailsTable.propTypes = {
+  allProducts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+};
