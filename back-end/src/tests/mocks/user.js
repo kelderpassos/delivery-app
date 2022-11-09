d4e66b6e566932385ec050f729094f54b2619f88
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-09 17:40:23 -0300

    test: User Controller - 100% de cobertura

--- back-end/src/tests/mocks/user.js
+++ back-end/src/tests/mocks/user.js
@@ -36,4 +36,8 @@ const mockCreate = {
   token: jwt.create({ id: mockUser.id, email: mockUser.email, role: mockUser.role }),
 }
 
-module.exports = { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller };
+const adminToken = jwt.create({ id: 1, email: 'admin@email.com', role: 'administrator' });
+const normalToken = jwt.create({ id: 1, email: 'customer@email.com', role: 'customer' });
+const invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJlbWFpbCI6ImN1c3RvbWVyQGVtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciJ9LCJpYXQiOjE2NjgwMjYxMjEsImV4cCI6MTY2ODExMjUyMX0.FH6LcE-KOgKxArxAsnQg28PZDH_yWe-XlBrc6izLipj'
+
+module.exports = { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller, adminToken, normalToken, invalidToken };
