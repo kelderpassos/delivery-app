e461e1277ee5cc08724577454094463919a74719
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-09 16:22:35 -0300

    test: User Service - 100% de cobertura

--- back-end/src/tests/mocks/user.js
+++ back-end/src/tests/mocks/user.js
@@ -1,6 +1,13 @@
 const md5 = require('md5');
 const jwt = require('../../auth/JWT');
 
+const mockNewUser = {
+  name: 'luizinho',
+  email: 'email@email.com',
+  password: '123456',
+  role: 'customer',
+}
+
 const mockUser = {
   id: 9,
   name: 'luizinho',
@@ -8,6 +15,11 @@ const mockUser = {
   password: md5('123456'),
   role: 'customer',
 };
+
+const mockAllSeller = [{
+  id: 8,
+  name: 'zezinho',
+}];
 
 const mockLogin = {
   id: 9,
@@ -17,4 +29,11 @@ const mockLogin = {
   token: jwt.create({ id: mockUser.id, email: mockUser.email, role: mockUser.role }),
 }
 
-module.exports = { mockUser, mockLogin };
+const mockCreate = {
+  name: 'luizinho',
+  email: 'email@email.com',
+  role: 'customer',
+  token: jwt.create({ id: mockUser.id, email: mockUser.email, role: mockUser.role }),
+}
+
+module.exports = { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller };
--- back-end/src/tests/unit/service/user.test.js
+++ back-end/src/tests/unit/service/user.test.js
@@ -2,7 +2,7 @@ const sinon = require('sinon');
 const chai = require('chai');
 const models = require('../../../database/models');
 const userService = require('../../../services/user.service');
-const { mockUser, mockLogin } = require('../../mocks/user');
+const { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller } = require('../../mocks/user');
 const CustomError = require('../../../helpers/CustomError');
 
 
@@ -10,14 +10,20 @@ const { User } = models;
 const { expect } = chai;
 
 describe('User Service', () => {
-  beforeEach(() => {
+  before(() => {
     sinon.stub(User, 'findOne')
       .onCall(0).resolves(mockUser)
       .onCall(1).resolves(mockUser)
-      .onCall(2).resolves({});
+      .onCall(2).resolves(null)
+      .onCall(3).resolves(mockUser)
+      .onCall(4).resolves(null)
+      .onCall(5).resolves(mockUser);
+
+    sinon.stub(User, 'create').resolves(mockUser);
+    sinon.stub(User, 'findAll').resolves(mockAllSeller)
   });
 
-  afterEach(() => {
+  after(() => {
     sinon.restore();
   });
 
@@ -42,8 +48,47 @@ describe('User Service', () => {
         error = err;
       }
 
+      expect(error.message).to.be.equal('User not found');
+      expect(error?.status).to.be.equal(404);
+    });
+
+    it('With failure - invalid password', async () => {
+      let error;
+      try {
+        await userService.login({ email: 'found@email.com', password: 'invalid' });
+      } catch (err) {
+        error = err;
+      }
+
       expect(error.message).to.be.equal('Invalid password');
       expect(error?.status).to.be.equal(400);
     });
   });
+
+  describe('Register a user', () => {
+    it('With success', async () => {
+      const result = await userService.customerRegister({ ...mockNewUser });
+      expect(result).to.be.deep.equal(mockCreate);
+    });
+
+    it('with failure - user already registered', async () => {
+      let error;
+
+      try {
+        await userService.customerRegister({ ...mockNewUser });
+      } catch (err) {
+        error = err;
+      }
+
+      expect(error.message).to.be.equal('Name or email already registered');
+      expect(error.status).to.be.equal(409);
+    });
+  });
+
+  describe('Get all sellers', () => {
+    it('With success', async () => {
+      const result = await userService.findSellersNames();
+      expect(result).to.be.deep.equal(mockAllSeller);
+    });
+  })
 });
