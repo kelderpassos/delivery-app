114487d3906b3feedf8fe7d0a0256b9b27038bc8
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-10 14:58:31 -0300

    fix: atualiza mock dos tokens

--- back-end/src/tests/mocks/user.js
+++ back-end/src/tests/mocks/user.js
@@ -36,8 +36,4 @@ const mockCreate = {
   token: jwt.create({ id: mockUser.id, email: mockUser.email, role: mockUser.role }),
 }
 
-const adminToken = jwt.create({ id: 1, email: 'admin@email.com', role: 'administrator' });
-const normalToken = jwt.create({ id: 1, email: 'customer@email.com', role: 'customer' });
-const invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJlbWFpbCI6ImN1c3RvbWVyQGVtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciJ9LCJpYXQiOjE2NjgwMjYxMjEsImV4cCI6MTY2ODExMjUyMX0.FH6LcE-KOgKxArxAsnQg28PZDH_yWe-XlBrc6izLipj'
-
-module.exports = { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller, adminToken, normalToken, invalidToken };
+module.exports = { mockUser, mockLogin, mockNewUser, mockCreate, mockAllSeller };
