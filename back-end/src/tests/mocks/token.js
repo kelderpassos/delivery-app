55f7398442f7f4b8d1079615f7cbb4064c557eec
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-11-10 15:16:42 -0300

    test: Sale Controller

--- back-end/src/tests/mocks/token.js
+++ back-end/src/tests/mocks/token.js
@@ -1,3 +1,5 @@
+const jwt = require('../../auth/JWT');
+
 const adminToken = jwt.create({ id: 1, email: 'admin@email.com', role: 'administrator' });
 const normalToken = jwt.create({ id: 1, email: 'customer@email.com', role: 'customer' });
 const invalidToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJlbWFpbCI6ImN1c3RvbWVyQGVtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciJ9LCJpYXQiOjE2NjgwMjYxMjEsImV4cCI6MTY2ODExMjUyMX0.FH6LcE-KOgKxArxAsnQg28PZDH_yWe-XlBrc6izLipj'
