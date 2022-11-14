bf1bf3fa95af64f7b9658378a6675210caf7aec1
Kelder Passos <kelder.passos@gmail.com>
2022-11-14 14:16:30 -0300

    Feat: homepage styling done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Admin from './pages/Admin';
 import Checkout from './pages/Checkout';
 import Login from './pages/Login';
@@ -9,11 +9,12 @@ import Products from './pages/Products';
 import Register from './pages/Register';
 import SellerOrders from './pages/SellerOrders';
 import SellerOrderDetails from './pages/SellerOrderDetails';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/" element={ <Navigate to="/login" /> } />
+      <Route exact path="/" element={ <Homepage /> } />
       <Route exact path="/login" element={ <Login /> } />
       <Route exact path="/register" element={ <Register /> } />
       <Route exact path="/customer/products" element={ <Products /> } />
